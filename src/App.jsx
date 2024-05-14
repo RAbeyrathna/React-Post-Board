e278ca78e3ca43854bcf0e9f9bb470488223f6d8
RAbeyrathna <rahal@res-network.com>
2024-05-14 17:53:54 +1000

    Add props to adjust Post content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Post from "./components/Post"
 
 function App() {
-  return <Post />;
+  return(
+    <main>
+      <Post author="Rahal" body="Hello world!"/>
+      <Post author="Nala" body="Bark!"/>
+    </main>
+  );
 }
 
 export default App;
