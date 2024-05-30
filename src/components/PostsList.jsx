83a8bab65bc88effb3105e78b5b9e6f5e01f8358
RAbeyrathna <rahal@res-network.com>
2024-05-30 13:06:11 +1000

    feat: Add modal with visibility for new post

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,11 +2,19 @@ import { useState } from 'react';
 
 import Post from "./Post";
 import NewPost from "./NewPost";
+import Modal from './Modal';
 import classes from "./PostsList.module.css";
 
+
 function PostsList(){
+    const [ modalIsVisible, setModalVisibility ] = useState(true);
+
     const [ enteredBody, setEnteredBody ] = useState('');
     const [ enteredAuthor, setEnteredAuthor ] = useState('');
+
+    function hideModalHandler(){
+        setModalVisibility(false);
+    }
 
     function changeBodyHandler(event){
         setEnteredBody(event.target.value);
@@ -18,7 +26,12 @@ function PostsList(){
 
     return(
         <>
-            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+            {modalIsVisible ? (
+                <Modal onClose={hideModalHandler}>
+                    <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+                </Modal>
+                ) : false}
+            
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody}/>
                 <Post author="Nala" body="Bark!"/>
