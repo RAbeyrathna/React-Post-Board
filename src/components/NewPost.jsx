a3ca66e81919dc70361e43695b98ff7317e8b2a3
RAbeyrathna <rahal@res-network.com>
2024-05-30 12:35:39 +1000

    feat: Add state for body text of post

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,18 +1,27 @@
+import { useState } from 'react';
+
 import classes from './NewPost.module.css';
 
 function NewPost() {
-  return (
-    <form className={classes.form}>
-      <p>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
-      </p>
-      <p>
-        <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
-      </p>
-    </form>
-  );
+    const [ enteredBody, setEnteredBody ] = useState('');
+
+    function changeBodyHandler(event){
+        setEnteredBody(event.target.value);
+    }
+
+    return (
+        <form className={classes.form}>
+        <p>
+            <label htmlFor="name">Your name</label>
+            <input type="text" id="name" required />
+        </p>
+        <p>
+            <label htmlFor="body">Text</label>
+            <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
+        </p>
+        <p>{enteredBody}</p>
+        </form>
+    );
 }
 
 export default NewPost;
