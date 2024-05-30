dc40404d10583925bca88955a3bdde3c7edce2ff
RAbeyrathna <rahal@res-network.com>
2024-05-30 13:13:13 +1000

    fix: Change modal visibility to if statement

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -12,6 +12,16 @@ function PostsList(){
     const [ enteredBody, setEnteredBody ] = useState('');
     const [ enteredAuthor, setEnteredAuthor ] = useState('');
 
+    let modalContent;
+
+    if (modalIsVisible){
+        modalContent = (
+        <Modal onClose={hideModalHandler}>
+            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+        </Modal>
+        );
+    }
+
     function hideModalHandler(){
         setModalVisibility(false);
     }
@@ -25,13 +35,16 @@ function PostsList(){
     }
 
     return(
-        <>
-            {modalIsVisible ? (
+        <> 
+            {/* Ternary Modal Visibility */}
+            {/* {modalIsVisible ? (
                 <Modal onClose={hideModalHandler}>
                     <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
                 </Modal>
                 ) : false}
-            
+             */}
+             
+            {modalContent}
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody}/>
                 <Post author="Nala" body="Bark!"/>
