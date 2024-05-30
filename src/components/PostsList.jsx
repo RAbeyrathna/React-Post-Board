edcbe31ed8db44c93974b4752b6a045e09b5e6b5
RAbeyrathna <rahal@res-network.com>
2024-05-30 12:48:32 +1000

    feat: Add state for author of post

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
+
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
 
 function PostsList(){
+    const [ enteredBody, setEnteredBody ] = useState('');
+    const [ enteredAuthor, setEnteredAuthor ] = useState('');
+
+    function changeBodyHandler(event){
+        setEnteredBody(event.target.value);
+    }
+
+    function changeAuthorHandler(event){
+        setEnteredAuthor(event.target.value);
+    }
+
     return(
         <>
-            <NewPost />
+            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
             <ul className={classes.posts}>
-                <Post author="Rahal" body="Hello world!"/>
+                <Post author={enteredAuthor} body={enteredBody}/>
                 <Post author="Nala" body="Bark!"/>
             </ul>
         </>
