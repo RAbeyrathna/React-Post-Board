d71b65291bd27ca0da17b260711717d4b59cf104
RAbeyrathna <rahal@res-network.com>
2024-05-30 14:12:45 +1000

    feat: Implement post handler for creating posts

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 import Post from "./Post";
 import NewPost from "./NewPost";
@@ -7,44 +7,26 @@ import classes from "./PostsList.module.css";
 
 
 function PostsList({isCreatingPost, onFinishedPost}){
+    const [posts, setPosts] = useState([]);
 
-
-    const [ enteredBody, setEnteredBody ] = useState('');
-    const [ enteredAuthor, setEnteredAuthor ] = useState('');
+    function addPostHandler(postData){
+        setPosts((existingPosts) => [postData, ...existingPosts]);
+    }
 
     let modalContent;
 
     if (isCreatingPost){
         modalContent = (
         <Modal onClose={onFinishedPost}>
-            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} onCancel={onFinishedPost} />
+            <NewPost onCancel={onFinishedPost} onAddPost={addPostHandler}/>
         </Modal>
         );
     }
 
-
-
-    function changeBodyHandler(event){
-        setEnteredBody(event.target.value);
-    }
-
-    function changeAuthorHandler(event){
-        setEnteredAuthor(event.target.value);
-    }
-
     return(
         <> 
-            {/* Ternary Modal Visibility */}
-            {/* {modalIsVisible ? (
-                <Modal onClose={hideModalHandler}>
-                    <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
-                </Modal>
-                ) : false}
-             */}
-
             {modalContent}
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody}/>
                 <Post author="Nala" body="Bark!"/>
             </ul>
         </>
